9591fbbce87b62fa50fb363120fbd65c56a0a3c4
gg134679852 <metal852794613@gmail.com>
2021-05-06 09:30:39 +0800

    Fixing deployment issue

--- public/movie-data.js
+++ public/movie-data.js
@@ -20,6 +20,10 @@ async function movieScraper() {
     console.log("載入網頁中...")
     const browser = await puppeteer.launch({
       headless: false,
+      'args': [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
     })
     const page = await browser.newPage()
 

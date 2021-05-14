d9dffc43e116723e602a5cd802ab08d43a5753ce
gg134679852 <metal852794613@gmail.com>
2021-05-14 20:03:43 +0800

    heroku PG switch to ClearDB mysql

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 global.Promise = Promise
 app.engine(
   'handlebars',

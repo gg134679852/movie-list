ebbc9e57c3f9d6231081d165107092e205825219
gg134679852 <metal852794613@gmail.com>
2021-05-20 00:19:11 +0800

    add-movie-loading-message

--- controllers/movieListControllers.js
+++ controllers/movieListControllers.js
@@ -1,11 +1,16 @@
 const { movieDatas, totalLength} = require('../public/movie-data')
 const movieListService = require('../services/movieListService')
+const loading = 'loading'
 
 const movieListControllers = {
   getMovie: (req, res) => {
-    const loading = 'loading'
     if (movieDatas.length === 0 || movieDatas.length !== totalLength[0]) {
-      res.render('movieList', { loading })
+      if (totalLength[0]> 0){
+        const movieDataLength = movieDatas.length
+        const totalmovieLength = totalLength[0]
+        res.render('movieList', { movieDataLength, totalmovieLength })
+      } else { 
+        res.render('movieList', { loading })}
       movieListService.getMovie(req, res, (data) => {
         const renderData = data.movieDatas
         res.render('movieList', {renderData})
